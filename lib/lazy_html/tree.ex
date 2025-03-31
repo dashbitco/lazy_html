d5d0adddddc4620aa62ae1b869f7a0391c65d940
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-03-31 18:17:47 +0900

    Align serialization with phoenix_html

--- lib/lazy_html/tree.ex
+++ lib/lazy_html/tree.ex
@@ -36,10 +36,6 @@ defmodule LazyHTML.Tree do
     # We build the html by continuously appending to a result binary.
     # Appending to a binary is optimised by the runtime, so this
     # approach is memory efficient.
-    #
-    # For HTML specifics, refer to the standard [1].
-    #
-    # [1]: https://html.spec.whatwg.org/multipage/parsing.html#serialising-html-fragments
 
     to_html(tree, true, <<>>)
   end
@@ -56,11 +52,12 @@ defmodule LazyHTML.Tree do
   defp to_html([{tag, attrs, children} | tree], escape, html) do
     html = <<html::binary, "<", tag::binary>>
     html = append_attrs(attrs, html)
-    html = <<html::binary, ">">>
 
     if tag in @void_tags do
+      html = <<html::binary, "/>">>
       to_html(tree, escape, html)
     else
+      html = <<html::binary, ">">>
       escape_children = tag not in @no_escape_tags
       html = to_html(children, escape_children, html)
       html = <<html::binary, "</", tag::binary, ">">>
@@ -71,7 +68,7 @@ defmodule LazyHTML.Tree do
   defp to_html([text | tree], escape, html) when is_binary(text) do
     html =
       if escape do
-        append_escaped(text, :content, html)
+        append_escaped(text, html)
       else
         <<html::binary, text::binary>>
       end
@@ -87,7 +84,7 @@ defmodule LazyHTML.Tree do
 
   defp append_attrs([{name, value} | attrs], html) do
     html = <<html::binary, " ", name::binary, ~S/="/>>
-    html = append_escaped(value, :attribute, html)
+    html = append_escaped(value, html)
     html = <<html::binary, ~S/"/>>
     append_attrs(attrs, html)
   end
@@ -96,61 +93,45 @@ defmodule LazyHTML.Tree do
   # Once we do, we take the whole text chunk up until that point and
   # we append it to the result. This is more efficient than appending
   # each untransformed character individually.
+  #
+  # Note that we apply the same escaping inside attribute values and
+  # tag contents. We could escape less by making it contextual, but
+  # we want to match the behaviour of Phoenix.HTML [1].
+  #
+  # [1]: https://github.com/phoenixframework/phoenix_html/blob/v4.2.1/lib/phoenix_html/engine.ex#L29-L35
 
-  defp append_escaped(text, mode, html) when mode in [:content, :attribute] do
-    append_escaped(text, text, 0, 0, mode, html)
+  defp append_escaped(text, html) do
+    append_escaped(text, text, 0, 0, html)
   end
 
-  defp append_escaped(<<>>, text, 0 = _offset, _size, _mode, html) do
+  defp append_escaped(<<>>, text, 0 = _offset, _size, html) do
     # We scanned the whole text and there were no characters to escape,
     # so we append the whole text.
     <<html::binary, text::binary>>
   end
 
-  defp append_escaped(<<>>, text, offset, size, _mode, html) do
+  defp append_escaped(<<>>, text, offset, size, html) do
     chunk = binary_part(text, offset, size)
     <<html::binary, chunk::binary>>
   end
 
-  defp append_escaped(<<?&, rest::binary>>, text, offset, size, mode, html) do
-    chunk = binary_part(text, offset, size)
-    html = <<html::binary, chunk::binary, "&amp;">>
-    append_escaped(rest, text, offset + size + 1, 0, mode, html)
-  end
+  escapes = [
+    {?&, "&amp;"},
+    {?<, "&lt;"},
+    {?>, "&gt;"},
+    {?", "&quot;"},
+    {?', "&#39;"}
+  ]
 
-  defp append_escaped(<<194, rest::binary>>, text, offset, size, mode, html) do
-    # We match the second byte separately, so that all main clauses
-    # match only a single byte, which is faster.
-    case rest do
-      <<160, rest::binary>> ->
-        chunk = binary_part(text, offset, size)
-        html = <<html::binary, chunk::binary, "&nbsp;">>
-        append_escaped(rest, text, offset + size + 2, 0, mode, html)
-
-      _other ->
-        append_escaped(rest, text, offset, size + 1, mode, html)
+  for {char, escaped} <- escapes do
+    defp append_escaped(<<unquote(char), rest::binary>>, text, offset, size, html) do
+      chunk = binary_part(text, offset, size)
+      html = <<html::binary, chunk::binary, unquote(escaped)>>
+      append_escaped(rest, text, offset + size + 1, 0, html)
     end
   end
 
-  defp append_escaped(<<?<, rest::binary>>, text, offset, size, :content, html) do
-    chunk = binary_part(text, offset, size)
-    html = <<html::binary, chunk::binary, "&lt;">>
-    append_escaped(rest, text, offset + size + 1, 0, :content, html)
-  end
-
-  defp append_escaped(<<?>, rest::binary>>, text, offset, size, :content, html) do
-    chunk = binary_part(text, offset, size)
-    html = <<html::binary, chunk::binary, "&gt;">>
-    append_escaped(rest, text, offset + size + 1, 0, :content, html)
-  end
-
-  defp append_escaped(<<?", rest::binary>>, text, offset, size, :attribute, html) do
-    chunk = binary_part(text, offset, size)
-    html = <<html::binary, chunk::binary, "&quot;">>
-    append_escaped(rest, text, offset + size + 1, 0, :attribute, html)
-  end
-
-  defp append_escaped(<<_char, rest::binary>>, text, offset, size, mode, html) do
-    append_escaped(rest, text, offset, size + 1, mode, html)
+  defp append_escaped(<<_char, rest::binary>>, text, offset, size, html) do
+    append_escaped(rest, text, offset, size + 1, html)
   end
 end
