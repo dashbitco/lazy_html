b9762df9633a89da67e68849853c12b83944456c
Giacomo Leidi <therewasa@fishinthecalculator.me>
2026-07-18 16:46:16 +0200

    mix.exs: Make elixir_make optional at runtime.
    elixir_make shouldn't be needed at runtime, this commit makes it
    optional.
    
    * mix.exs (deps): Make elixir_make optional at runtime.

--- mix.exs
+++ mix.exs
@@ -75,7 +75,7 @@ defmodule LazyHTML.MixProject do
   defp deps do
     [
       {:fine, "~> 0.1.0"},
-      {:elixir_make, "~> 0.9"},
+      {:elixir_make, "~> 0.9", runtime: false},
       {:cc_precompiler, "~> 0.1", runtime: false},
       {:ex_doc, "~> 0.36", only: :dev, runtime: false}
     ]
