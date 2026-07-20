abbd697f782f3208de17a5bcaa99ec904b4da7a2
Jonatan Kłosko <jonatanklosko@gmail.com>
2026-07-20 12:22:50 +0200

    Apply suggestion from @jonatanklosko

--- mix.exs
+++ mix.exs
@@ -74,7 +74,7 @@ defmodule LazyHTML.MixProject do
 
   defp deps do
     [
-      {:fine, "~> 0.1.0"},
+      {:fine, "~> 0.1.0", runtime: false},
       {:elixir_make, "~> 0.9", runtime: false},
       {:cc_precompiler, "~> 0.1", runtime: false},
       {:ex_doc, "~> 0.36", only: :dev, runtime: false}
